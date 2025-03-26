7520d674ec625aabd2164da35c6023522a10b160
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-03-26 14:00:13 -0300

    Add custom tabbar

---
+++ AppStore/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  AppStore
+//
+//  Created by Dmitry Volkov on 26/03/2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ AppStore/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  AppStore
+//
+//  Created by Dmitry Volkov on 26/03/2025.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [createNavController(viewController: HomeViewController(), title: "Home", imageName: "trash.circle"),
+                           createNavController(viewController: HomeViewController(), title: "Red", imageName: "pencil.tip.crop.circle"),
+                           createNavController(viewController: HomeViewController(), title: "Blue", imageName: "rectangle.portrait.and.arrow.right.fill")]
+    }
+    
+    
+    
+    private func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        viewController.navigationItem.title = title
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(systemName: imageName)
+        return navController
+    }
+}
